7b1baa1ac0ea0678d68fe8080e87479f6ab9253a
Henry Hartzler <hartzler.henry@gmail.com>
2022-05-26 20:06:39 -0600

    tests the time at dawn for range errors

--- test/index_test.js
+++ test/index_test.js
@@ -16,5 +16,40 @@ describe('defines a rooster', () => {
   
     });
   });
+  describe('timeAtDawn', () => {
+    it('returns its arugment as a string', () => {
+    // Setup
+    const inputNum = 12;
+    const expected = '12';
+    // Exercise: write to file
+   const result = Rooster.timeAtDawn(inputNum);
+
+    // Verify: compare file contents to string
+    assert.strictEqual(expected, result);
+
+    // Teardown: delete path
   
+    });
+    it('throws an error if passed a number less than 0', () => {
+      // Setup
+      const inputNum = -1;
+      //Exercise
+      //the next part to verify should throw an error
+      //Verify
+      assert.throws(() => {
+        Rooster.timeAtDawn(inputNum);
+      },
+      RangeError);
+    });
+    it('throws an error if passed a number greater than 23', () => {
+      //Setup
+      const inputNum = 24;
+      //Exercise: next part should throw an error
+      //Verify
+      assert.throws(() => {
+        Rooster.timeAtDawn(inputNum);
+      },
+      RangeError);
+    });
+  });
 }); 
